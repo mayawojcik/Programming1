218abe35acc210cae8dfee32382ba84d64173593
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-03-26 10:28:25 -0500

    3.26.24 :(

---
+++ prog122d/prog122d/MainForm.py
@@ -0,0 +1,95 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._listBox1 = System.Windows.Forms.ListBox()
+		self._button1 = System.Windows.Forms.Button()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# listBox1
+		# 
+		self._listBox1.BackColor = System.Drawing.Color.Navy
+		self._listBox1.Font = System.Drawing.Font("Microsoft YaHei", 11.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._listBox1.ForeColor = System.Drawing.Color.FromArgb(192, 192, 255)
+		self._listBox1.FormattingEnabled = True
+		self._listBox1.ItemHeight = 20
+		self._listBox1.Location = System.Drawing.Point(12, 12)
+		self._listBox1.Name = "listBox1"
+		self._listBox1.Size = System.Drawing.Size(442, 424)
+		self._listBox1.TabIndex = 0
+		# 
+		# button1
+		# 
+		self._button1.BackColor = System.Drawing.Color.Navy
+		self._button1.Font = System.Drawing.Font("Microsoft YaHei", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.ForeColor = System.Drawing.Color.FromArgb(192, 192, 255)
+		self._button1.Location = System.Drawing.Point(461, 25)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(192, 122)
+		self._button1.TabIndex = 1
+		self._button1.Text = "calculator"
+		self._button1.UseVisualStyleBackColor = False
+		self._button1.Click += self.Button1Click
+		# 
+		# button2
+		# 
+		self._button2.BackColor = System.Drawing.Color.Navy
+		self._button2.Font = System.Drawing.Font("Microsoft YaHei", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.ForeColor = System.Drawing.Color.FromArgb(192, 192, 255)
+		self._button2.Location = System.Drawing.Point(461, 162)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(192, 122)
+		self._button2.TabIndex = 2
+		self._button2.Text = "clear"
+		self._button2.UseVisualStyleBackColor = False
+		self._button2.Click += self.Button2Click
+		# 
+		# button3
+		# 
+		self._button3.BackColor = System.Drawing.Color.Navy
+		self._button3.Font = System.Drawing.Font("Microsoft YaHei", 15.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.ForeColor = System.Drawing.Color.FromArgb(192, 192, 255)
+		self._button3.Location = System.Drawing.Point(461, 300)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(192, 122)
+		self._button3.TabIndex = 3
+		self._button3.Text = "exit"
+		self._button3.UseVisualStyleBackColor = False
+		self._button3.Click += self.Button3Click
+		# 
+		# MainForm
+		# 
+		self.BackColor = System.Drawing.Color.FromArgb(128, 128, 255)
+		self.ClientSize = System.Drawing.Size(665, 459)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._button1)
+		self.Controls.Add(self._listBox1)
+		self.Name = "MainForm"
+		self.Text = "prog122d"
+		self.ResumeLayout(False)
+
+
+	def Button2Click(self, sender, e):
+		self._textBox1.Items.Clear()
+
+	def Button3Click(self, sender, e):
+		self._textBox1.Items.Exit()
+
+	def Button1Click(self, sender, e):
+		heading = "x\t\ty"
+		self._listBox1.Items.Add(heading)
+		for num in range(-12, 16):
+			X = num
+			Y = 1
+			msg = str(X) + "\t\t" + str(Y)
+			self._listBox1.Items.Add(msg)
