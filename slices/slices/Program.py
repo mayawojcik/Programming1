c0502b253f4fcd5c373379f32983b228d990b81f
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-04-15 10:11:02 -0500

    4.15.24

---
+++ slices/slices/Program.py
@@ -0,0 +1,17 @@
+﻿list1 = [1, 2, 3, 4, 5]
+list2 = list1[0:3]  # [:3]
+print(list2)
+list3 = list1[3:len(list1)]  # [3:]
+print(list3)
+print((list2 + list3) == list1) # equals all of list 1
+print("")
+
+strHi = "Hello, world!"
+substr = strHi[:5]  # [0:5]
+print(substr)
+print(strHi[-1])
+print(strHi[-6:-1])  # [start:stop:step]
+print(strHi[::-1])  # [0:len(strHi):-1]
+print(strHi == strHi[::-1])
+
+input()
