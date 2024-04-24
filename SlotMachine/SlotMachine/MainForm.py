ba5c19482b5300adee329d37c2a6f11bd8c0138a
mayawojcik <162478140+mayawojcik@users.noreply.github.com>
2024-04-24 10:37:59 -0500

    4.24.24

--- SlotMachine/SlotMachine/MainForm.py
+++ SlotMachine/SlotMachine/MainForm.py
@@ -86,6 +86,7 @@ class MainForm(Form):
 		self._pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
 		self._pictureBox4.TabIndex = 3
 		self._pictureBox4.TabStop = False
+		self._pictureBox4.Visible = False
 		# 
 		# button1
 		# 
@@ -146,11 +147,14 @@ class MainForm(Form):
 		# label3
 		# 
 		self._label3.BackColor = System.Drawing.Color.White
+		self._label3.Font = System.Drawing.Font("Microsoft YaHei", 14.25, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
 		self._label3.ForeColor = System.Drawing.Color.FromArgb(255, 192, 255)
 		self._label3.Location = System.Drawing.Point(464, 302)
 		self._label3.Name = "label3"
 		self._label3.Size = System.Drawing.Size(100, 31)
 		self._label3.TabIndex = 9
+		self._label3.Text = "100"
+		self._label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
 		# 
 		# progressBar1
 		# 
@@ -171,6 +175,7 @@ class MainForm(Form):
 		self._pictureBox5.Size = System.Drawing.Size(48, 50)
 		self._pictureBox5.TabIndex = 11
 		self._pictureBox5.TabStop = False
+		self._pictureBox5.Visible = False
 		# 
 		# pictureBox6
 		# 
@@ -181,6 +186,7 @@ class MainForm(Form):
 		self._pictureBox6.Size = System.Drawing.Size(48, 50)
 		self._pictureBox6.TabIndex = 12
 		self._pictureBox6.TabStop = False
+		self._pictureBox6.Visible = False
 		# 
 		# pictureBox7
 		# 
@@ -191,6 +197,7 @@ class MainForm(Form):
 		self._pictureBox7.Size = System.Drawing.Size(48, 50)
 		self._pictureBox7.TabIndex = 13
 		self._pictureBox7.TabStop = False
+		self._pictureBox7.Visible = False
 		# 
 		# pictureBox8
 		# 
@@ -201,6 +208,7 @@ class MainForm(Form):
 		self._pictureBox8.Size = System.Drawing.Size(48, 50)
 		self._pictureBox8.TabIndex = 14
 		self._pictureBox8.TabStop = False
+		self._pictureBox8.Visible = False
 		# 
 		# pictureBox9
 		# 
@@ -211,6 +219,7 @@ class MainForm(Form):
 		self._pictureBox9.Size = System.Drawing.Size(48, 50)
 		self._pictureBox9.TabIndex = 15
 		self._pictureBox9.TabStop = False
+		self._pictureBox9.Visible = False
 		# 
 		# pictureBox10
 		# 
@@ -221,15 +230,22 @@ class MainForm(Form):
 		self._pictureBox10.Size = System.Drawing.Size(48, 50)
 		self._pictureBox10.TabIndex = 16
 		self._pictureBox10.TabStop = False
+		self._pictureBox10.Visible = False
 		# 
 		# pictureBox11
 		# 
+		self._pictureBox11.BackgroundImage = resources.GetObject("pictureBox11.BackgroundImage")
 		self._pictureBox11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
 		self._pictureBox11.Location = System.Drawing.Point(94, 251)
 		self._pictureBox11.Name = "pictureBox11"
 		self._pictureBox11.Size = System.Drawing.Size(48, 50)
 		self._pictureBox11.TabIndex = 17
 		self._pictureBox11.TabStop = False
+		self._pictureBox11.Visible = False
+		# 
+		# timer1
+		# 
+		self._timer1.Tick += self.Timer1Tick
 		# 
 		# MainForm
 		# 
@@ -255,6 +271,7 @@ class MainForm(Form):
 		self.Controls.Add(self._pictureBox1)
 		self.Name = "MainForm"
 		self.Text = "SlotMachine"
+		self.Load += self.MainFormLoad
 		self._pictureBox1.EndInit()
 		self._pictureBox2.EndInit()
 		self._pictureBox3.EndInit()
@@ -309,7 +326,102 @@ class MainForm(Form):
 		elif bet > money and bet > money2:
 			MessageBox.Show("You don't have enough money!")
 		else:
-			# TODO
+			self._button1.BackgroundImage = LevOn
+			self._pictureBox4.Visible = True
+			self._timer1.Enabled = True
+			self._label3.Text = str(round(money2, 2))
+			self._prgoressBar1.Value = 0
+			
+			num1 = self.num1
+			num2 = self.num2
+			num3 = self.num3
+			
+			if num1 == 1 and num2 == 1 and num3 == 1:
+				money2 += bet * 2
+			if num1 == 2 and num2 == 2 and num3 == 2:
+				money2 += bet * 20
+			if num1 == 3 and num2 == 3 and num3 == 3:
+				money2 += bet * 30
+			if num1 == 4 and num2 == 4 and num3 == 4:
+				money2 += bet * 40
+			if num1 == 5 and num2 == 5 and num3 == 5:
+				money2 += bet * 100
+				
+			self.num1 = 0
+			self.num2 = 0
+			self.num3 = 0
+			self._label3.Text = str(round(money2, 2))
+			
+			if money2 <= 0:
+				MessageBox.Show("You ran out of cash!")
+			
+		
 	
 		
+		pass
+
+	def Timer1Tick(self, sender, e):
+		im1 = self._pictureBox5.BackgroundImage
+		im2 = self._pictureBox6.BackgroundImage
+		im3 = self._pictureBox7.BackgroundImage
+		im4 = self._pictureBox8.BackgroundImage
+		im5 = self._pictureBox9.BackgroundImage
+		LevOff = self._pictureBox10.BackgroundImage
+		LevOn = self._pictureBox11.BackgroundImage
+		rnd = System.Random()
+		num1 = 0
+		num2 = 0
+		num3 = 0
+		
+		pb1 = self._pictureBox1
+		pb2 = self._pictureBox2
+		pb3 = self._pictureBox3
+		
+		for lcv in range(0, 1000):
+			num1 = rnd.Next(1,6) # Generate number between 1-5
+			num2 = rnd.Next(1,6)
+			num3 = rnd.Next(1,6)
+			
+			# copy/paste this for num2/pb2 and num3/pb3
+			if num1 == 1:
+				pb1.BackgroundImage = im1
+			elif num1 == 2:
+				pb1.BackgroundImage = im2
+			elif num1 == 3:
+				pb1.BackgroundImage = im3
+			elif num1 == 4:
+				pb1.BackgroundImage = im4
+			elif num1 == 5:
+				pb1.BackgroundImage = im5
+				
+			if num2 == 1:
+				pb2.BackgroundImage = im1
+			elif num2 == 2:
+				pb2.BackgroundImage = im2
+			elif num2 == 3:
+				pb2.BackgroundImage = im3
+			elif num2 == 4:
+				pb2.BackgroundImage = im4
+			elif num2 == 5:
+				pb2.BackgroundImage = im5
+				
+			if num3 == 1:
+				pb3.BackgroundImage = im1
+			elif num3 == 2:
+				pb3.BackgroundImage = im2
+			elif num3 == 3:
+				pb3.BackgroundImage = im3
+			elif num3 == 4:
+				pb3.BackgroundImage = im4
+			elif num3 == 5:
+				pb3.BackgroundImage = im5
+			
+			self._progressBar1.Increment(1)
+			if self._progressBar1.Value == self._progressBar1.Maximum:
+				self._timer1.Enabled = False
+				self._pictureBox4.Visible = False
+				self._button1.BackgroundImage = LevOff
+		pass
+
+	def MainFormLoad(self, sender, e):
 		pass
